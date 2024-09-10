a32bb8d97e39a42249835eab0c23ddf47d2dbf6c
WilliamHFrench <144379928+WilliamHFrench@users.noreply.github.com>
2024-09-10 15:44:38 -0400

    Add files via upload

---
+++ CSE2102_Lab1/ElectricInterface.java
@@ -0,0 +1,14 @@
+
+
+public interface ElectricInterface {
+
+
+    double calcMPGe();
+    void setElectricMiles(double totalElectricMiles);
+    void setTotalkWh(double totalkWh);
+    void setCostPerkWh(double priceKWh);
+    double getCostPerkWh();
+
+}
+
+
